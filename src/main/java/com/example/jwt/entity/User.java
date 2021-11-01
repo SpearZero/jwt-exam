25478e2301d8997456d90e36880fee2598f26a08
SpearZero <41422767+SpearZero@users.noreply.github.com>
2021-11-01 13:06:35 +0900

    사용자 권한별 접근 구현

--- src/main/java/com/example/jwt/entity/User.java
+++ src/main/java/com/example/jwt/entity/User.java
@@ -32,7 +32,7 @@ public class User {
     @Size(max = 120)
     private String password;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "user_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
